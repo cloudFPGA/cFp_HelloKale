247c34d7cde575d5be11bb08aa81c128cad04bb6
Francois Abel <fab@zurich.ibm.com>
2019-09-06 18:12:18 +0200

    Add consistency to scripts.

--- ROLE/hls/mem_test_flash/run_hls.tcl
+++ ROLE/hls/mem_test_flash/run_hls.tcl
@@ -2,8 +2,8 @@
 # *                            cloudFPGA
 # *            All rights reserved -- Property of IBM
 # *----------------------------------------------------------------------------
-# * Created : Jun 2017
-# * Authors : Burkhard Ringlein (NGL@zurich.ibm.com)
+# * Created : Jan 2018
+# * Authors :  Francois Abel, Burkhard Ringlein
 # * 
 # * Description : A Tcl script for the HLS batch syhthesis 
 # * 
@@ -43,9 +43,10 @@ set tbDir        ${currDir}/tb
 
 # Get targets out of env  
 #-------------------------------------------------
-
-set hlsSim $env(hlsSim)
-set hlsCoSim $env(hlsCoSim)
+set hlsSim       $::env(hlsCSim)
+set hlsCSynth    $::env(hlsCSynth)
+set hlsCoSim     $::env(hlsCoSim)
+set hlsRtl       $::env(hlsRtl)
 
 # Open and Setup Project
 #-------------------------------------------------
@@ -53,8 +54,8 @@ open_project  ${projectName}_prj
 set_top       ${appName}_main
 #set_top       mpi_wrapper
 
-
-
+# Add files
+#-------------------------------------------------
 #for DEBUG flag 
 #if { $hlsSim || $hlsCoSim } { 
 #  add_files     ${srcDir}/${appName}.cpp -cflags "-DCOSIM -DDEBUG"
@@ -72,28 +73,47 @@ set_top       ${appName}_main
 #since DEBUG flag won't work....
 add_files ${srcDir}/dynamic.hpp
 
+# Create a solution
+#-------------------------------------------------
 open_solution ${solutionName}
 
 set_part      ${xilPartName}
 create_clock -period 6.4 -name default
 
+# Set reset behavior to control mode (see UG902)
+#-------------------------------------------------
+config_rtl -reset control
+
+# Disable start propagation FIFOs (see UG902)
+#-------------------------------------------------
+# [TODO - Check vivado_hls version and only enable this command if >= 2018]
+# config_rtl -disable_start_propagation
+
 # Run C Simulation and Synthesis
 #-------------------------------------------------
-
 if { $hlsSim } { 
   csim_design -compiler gcc -clean
-} else {
-
-  csynth_design
-  
-  if { $hlsCoSim } {
-    cosim_design -compiler gcc -trace_level all 
-  } else {
-  
-  # Export RTL
-  #-------------------------------------------------
-    export_design -rtl vhdl -format ${ipPkgFormat} -library ${ipLibrary} -display_name ${ipDisplayName} -description ${ipDescription} -vendor ${ipVendor} -version ${ipVersion}
-  }
 }
 
+# Run C Synthesis (refer to UG902)
+#-------------------------------------------------
+if { $hlsCSynth} { 
+    csynth_design
+}
+
+# Run C/RTL CoSimulation (refer to UG902)
+#-------------------------------------------------
+if { $hlsCoSim } {
+    cosim_design -compiler gcc -trace_level all
+}
+  
+# Export RTL (refer to UG902)
+#   -format ( sysgen | ip_catalog | syn_dcp )
+#-------------------------------------------------
+if { $hlsRtl } {
+    export_design -rtl vhdl -format ${ipPkgFormat} -library ${ipLibrary} -display_name ${ipDisplayName} -description ${ipDescription} -vendor ${ipVendor} -version ${ipVersion}
+}
+
+# Exit Vivado HLS
+#--------------------------------------------------
 exit
