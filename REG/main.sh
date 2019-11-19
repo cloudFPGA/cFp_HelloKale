3fefc3717a02131f04b3e473ccdc93fca781f44f
Francois Abel <fab@zurich.ibm.com>
2019-11-19 15:14:30 +0100

    Updates main.sh

--- REG/main.sh
+++ REG/main.sh
@@ -10,11 +10,22 @@
 #  *        Main regressions script. This script is called by e.g. the Jenkins server.
 #  *
 
-# ATTENTION: This script EXPECTS THAT IT IS EXECUTED FROM THE REPOISOTORY ROOT AND ALL NECESSARY ENVIROMENT IS SOURCED! 
-# $root should bet set externally
+# ATTENTION: 
+#   This script EXPECTS THAT IT IS EXECUTED FROM THE REPOSITORY ROOT AND ALL NECESSARY
+#    ENVIROMENT IS SOURCED!
+#   And $root should bet set externally.
 
-# STEP 0: we need to set the right environment
+# @brief A function to check if previous step passed.
+# @param[in] $1 the return value of the previous command.
+function exit_on_error {
+    if [ $1 -ne 0 ]; then
+        echo "EXIT ON ERROR: Regression \'$0\' FAILED."
+        echo "  Last return value was $1."
+        exit $1
+    fi
+}
 
+# STEP 0: We need to set the right environment
 export rootDir=$root
 export cFpRootDir="$rootDir/"
 export cFpIpDir="$rootDir/ip/"
@@ -30,12 +41,12 @@ export roleName2="unused"
 echo "set cFp environment"
 retval=1
 
-# STEP-1: make a monolithic build
-echo "======== START of STEP-1 ========"
+echo "======== START of STEP-1: Build \'monolithic\' ========"
 cd $root
 make monolithic
-retval=$? # saving return value
-echo "======== END of STEP-1 ========"
+exit_on_error $? 
+echo "======== END of STEP-1: Build \'monolithic\' ========"
 
-exit $retval
+
+exit 0
 
