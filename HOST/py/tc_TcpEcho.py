29f026096662208f96c87b769251067ba01aec6b
Francois Abel <fab@zurich.ibm.com>
2020-01-16 15:10:03 +0100

    Creating a few methods. [ci skip]

--- HOST/py/tc_TcpEcho.py
+++ HOST/py/tc_TcpEcho.py
@@ -21,7 +21,6 @@ import random
 import requests
 import socket
 import string
-import sys
 import threading
 import time
 
@@ -47,11 +46,12 @@ def str_rand_gen(size):
     return msg.encode()
 
 
-def tcp_tx(sock, message, count):
+def tcp_tx(sock, message, count, verbose=False):
     """TCP Tx Thread.
     :param sock,       the socket to send to.
     :param message,    the random string to sent.
     :param count,      the number of segments to send.
+    :param verbose,    enables verbosity.
     :return None"""
     print("The following message of %d bytes will be sent out %d times:\n  Message=%s\n" % (len(message), count, message.decode()))
     c = 0
@@ -68,80 +68,44 @@ def tcp_tx(sock, message, count):
     print("##################################################")
     print("#### TCP TX DONE with bandwidth = %6.1f Mb/s ####" % bandwidth)
     print("##################################################")
+    print()
 
 
-def tcp_rx(sock, message, count):
+def tcp_rx(sock, message, count, verbose):
     """TCP Rx Thread.
      :param sock,       the socket to receive from.
      :param message,    the expected string message to be received.
      :param count,      the number of segment to receive.
+     :param verbose,    enables verbosity.
      :return None"""
     loop = 0
     byteCnt = 0
     nrErr = 0
+    startTime = datetime.datetime.now()
     while byteCnt < count*len(message):
         try:
             data = sock.recv(len(message))
             byteCnt += len(data)
-            print("Loop=%d | RxBytes=%d" % (loop, byteCnt))
-            #if data == message:
-            #    print("Loop=%d | RxBytes=%d" % (loop, byteCnt))
-            #else:
-            #    print(" KO | Received %d bytes: Message=%s" % (len(data), data.decode()))
-            #    # print("    | Was expecting    : Message=%s" % message)
-            #    nrErr += 1
+            if verbose:
+                print("Loop=%d | RxBytes=%d" % (loop, byteCnt))
+                # if data == message:
+                #     print("Loop=%d | RxBytes=%d" % (loop, byteCnt))
+                # else:
+                #    print(" KO | Received  Message=%s" % data.decode())
+                #     print("    | Expecting Message=%s" % message)
+                #     nrErr += 1
+        except socket.error as exc:
+            print("[EXCEPTION] Socket error while receiving :: %s" % exc)
         finally:
             pass
         loop += 1
+    endTime = datetime.datetime.now()
+    elapseTime = endTime - startTime;
+    bandwidth  = len(message) * 8 * count * 1.0 / (elapseTime.total_seconds() * 1024 * 1024)
+    print("##################################################")
+    print("#### TCP RX DONE with bandwidth = %6.1f Mb/s ####" % bandwidth)
+    print("##################################################")
     print()
-    print("########################################")
-    print("#### TCP RX DONE with %4d error(s) ####" % nrErr)
-    print("########################################")
-
-
-def ipStrToList(ipAddrStr):
-    """Convert an IP address from dotted string to a list.
-    :return a list of integers.
-    """
-    ipAddrList = []
-    # Split the dotted IP address using the 'dot' separator
-    strFieldList = ipAddrStr.split(".")
-    # Turn the dotted IP address into a list
-    for o in range(0, len(strFieldList)):
-        try:
-            octet = int(strFieldList[o])
-        except ValueError as ex:
-            break
-        else:
-            ipAddrList.append(octet)
-    return ipAddrList
-
-def read_instance_id_from_console():
-    """Read and return an instance Id from the console.
-    :return the instance Id as an integer.
-    """
-    while True:
-        instId = input()
-        if not 1 <= int(instId) <= 32:
-            print("ERROR: Bad format for the instance Id.")
-            print("\tEnter a new instance Id in the range 1-32.\n")
-        else:
-            break
-    return int(instId)
-
-def read_ipv4_from_console():
-    """Read and return an IPv4 address from the console.
-    :return the IPV4 as a list on integers.
-    """
-    while True:
-        ipAddrList = []
-        ipAddrStr = input()
-        ipAddrList = ipStrToList(ipAddrStr)
-        if len(ipAddrList) != 4:
-            print("ERROR: Bad format for the IPv4 address.")
-        else:
-            break
-    return ipAddrList
 
 
 def getFpgaIpv4(args):
@@ -162,6 +126,128 @@ def getFpgaIpv4(args):
             return ipFpga
 
 
+def getFpgaPort(args):
+    """Retrieve the TCP destination port of the FPGA.
+    :param args, the options passed as arguments to the script.
+    :return the TCP port number as an integer."""
+    portFpga = args.fpga_port
+    if portFpga != 8803:
+        print("[ERROR] The current version of the cFp_BringUp role always listens on port #8803.\n")
+        exit(1)
+    return portFpga
+
+
+def getResourceManagerIpv4(args):
+    """Retrieve the IP address of the cloudFPGA Resource Manager.
+    :param args, the options passed as arguments to the script.
+    :return the IP address as an ipaddress.IPv4Address."""
+    ipResMngrStr = args.mngr_ipv4
+    while True:
+        if args.mngr_ipv4 == '':
+            # Take an IP address from the console
+            print("Enter the IPv4 address of the cloudFPGA Resource Manager (e.g. 10.12.0.132)")
+            ipResMngrStr = input()
+        try:
+            ipResMngr = ipaddress.ip_address(ipResMngrStr)
+        except ValueError:
+            print('[ERROR] Unrecognized IPv4 address.')
+        else:
+            return ipResMngr
+
+
+def getResourceManagerPort(args):
+    """Retrieve the TCP port of the cloudFPGA Resource Manager.
+    :param args, the options passed as arguments to the script.
+    :return the TCP port number as an integer."""
+    portMngr = args.mngr_port
+    if portMngr != 8080:
+        print("[ERROR] The current version of the cloudFPGA Resource manager always listens on port #8080.\n")
+        exit(1)
+    return portMngr
+
+
+def getInstanceId(args):
+    """Retrieve the instance Id that was assigned by the cloudFPGA Resource Manager.
+    :param args, the options passed as arguments to the script.
+    :return the instance Id as an integer."""
+    instId = args.inst_id
+    while True:
+        if not 1 <= args.inst_id <= 32:
+            # Take an instance Id from the console
+            print("Enter the instance Id that was assigned by the cloudFPGA Resource Manager (e.g. 1-32)")
+            instIdStr = input()
+            try:
+                instId = int(instIdStr)
+            except ValueError:
+                print("ERROR: Bad format for the instance Id.")
+                print("\tEnter a new instance Id in the range 1-32.\n")
+        else:
+            break
+    return instId
+
+
+def restartApp(instId, ipResMngr, portResMngr, user_name, user_passwd):
+    """Trigger the role of an FPGA to restart (i.e. perform a SW reset of the role)
+    :param instId:      the instance Id to restart (must be 1-32).
+    :param ipResMngr:   the IPv4 address of the cF resource manager.
+    :param portResMngr: the TCP port number of the cF resource manager.
+    :param user_name:   the user name as used to log in ZYC2.
+    :param user_passwd: the ZYC2 password attached to the user name.
+    :return: nothing
+    """
+    print("\nNow: Requesting the application of FPGA instance #%d to restart." % instId)
+    try:
+        # We must build a request that is formatted as follows:
+        #  http://10.12.0.132:8080/instances/13/app_restart?username=fab&password=secret
+        reqUrl = "http://" + str(ipResMngr) + ":" + str(portResMngr) + "/instances/" \
+                 + str(instId) + "/app_restart?username=" + user_name \
+                 + "&password=" + user_passwd
+        # DEBUG print("Generated request URL = ", reqUrl)
+        r1 = requests.patch(reqUrl)
+        print(r1.content.decode())
+    except Exception as e:
+        print("ERROR: Failed to reset the FPGA role")
+        print(str(e))
+        exit(1)
+
+
+def pingFpga(ipFpga):
+    """Ping an FPGA.
+    :param ipFpga: the IPv4 address of the FPGA.
+    :return: nothing
+    """
+    print("Now: Trying to \'ping\' the FPGA: ")
+    rc = os.system("ping -c 2 " + str(ipFpga))
+    if rc != 0:
+        print("[ERROR] FPGA does not reply to \'ping\'!")
+        exit(1)
+
+
+def waitUntilSocketPairCanBeReused(ipFpga, portFpga):
+    """Check and wait until the a socket pair can be reused.
+        [INFO] When a client or a server initiates an active close, then the same destination socket
+          (i.e. the same IP address / TCP port number) cannot be re-used immediately because
+          of security issues. Therefore, a closed connection must linger in a 'TIME_WAIT' or
+          'FIN_WAIT' state for as long as 2xMSL (Maximum Segment Lifetime), which corresponds
+          to twice the time a TCP segment might exist in the internet system. The MSL is
+          arbitrarily defined to be 2 minutes long.
+    :param ipFpga:   the IP address of FPGA.
+    :param portFpga: the TCP port of the FPGA.
+    :return: nothing
+    """
+    wait = True
+    # NETSTAT example: rc = os.system("netstat | grep '10.12.200.163:8803' | grep TIME_WAIT")
+    cmdStr = "netstat | grep \'" + str(ipFpga) + ":" + str(portFpga) + "\' | grep \'TIME_WAIT\|FIN_WAIT\' "
+    while wait:
+        rc = os.system(cmdStr)
+        if rc == 0:
+            print("[INFO] Cannot reuse this socket as long as it is in the \'TIME_WAIT\' or \'FIN_WAIT\' state.")
+            print("       Let's sleep for 5 sec...")
+            time.sleep(5)
+        else:
+            wait = False
+
+
 ###############################################################################
 #                                                                             #
 #                                 MAIN                                        #
@@ -171,78 +257,50 @@ def getFpgaIpv4(args):
 #  STEP-1: Parse the command line strings into Python objects
 # -----------------------------------------------------------------------------
 parser = argparse.ArgumentParser(description='A script to send/receive TCP data to/from an FPGA module.')
-parser.add_argument('-fi', '--fpga_ipv4',  type=str, default='',
+parser.add_argument('-fi', '--fpga_ipv4',   type=str, default='',
                            help='The destination IPv4 address of the FPGA (a.k.a image_ip / e.g. 10.12.200.163)')
-parser.add_argument('-fp', '--fpga_port',  type=int, default=8803,
+parser.add_argument('-fp', '--fpga_port',   type=int, default=8803,
                            help='The TCP destination port of the FPGA (default is 8803)')
-parser.add_argument('-ii', '--inst_id',    type=int, default=0,
+parser.add_argument('-ii', '--inst_id',     type=int, default=0,
                            help='The instance ID assigned by the cloudFPGA Resource Manager (range is 1-32)')
-parser.add_argument('-lc', '--loop_count', type=int, default=10,
+parser.add_argument('-lc', '--loop_count',  type=int, default=10,
                            help='The number of times to run run the test (default is 10)')
-parser.add_argument('-mi', '--mngr_ipv4',  type=str, default='10.12.0.132',
+parser.add_argument('-mi', '--mngr_ipv4',   type=str, default='10.12.0.132',
                            help='The IP address of the cloudFPGA Resource Manager (default is 10.12.0.132)')
-parser.add_argument('-mp', '--mngr_port',  type=int, default=8080,
+parser.add_argument('-mp', '--mngr_port',   type=int, default=8080,
                            help='The TCP port of the cloudFPGA Resource Manager (default is 8080)')
-parser.add_argument('-pw', '--password',   type=str, default='',
-                           help='The ZYC2 password attached to the user name')
-parser.add_argument('-un', '--user_name',  type=str, default='',
+parser.add_argument('-un', '--user_name',   type=str, default='',
                            help='A user name as used to log in ZYC2 (.e.g \'fab\')')
-parser.add_argument('-v',  '--verbose',    action="store_true",
+parser.add_argument('-up', '--user_passwd', type=str, default='',
+                           help='The ZYC2 password attached to the user name')
+parser.add_argument('-v',  '--verbose',     action="store_true",
                            help='Enable verbosity')
 
 args = parser.parse_args()
 
-if args.user_name == '' or args.password == "":
+if args.user_name == '' or args.user_passwd == '':
     print("\nWARNING: You must provide a ZYC2 user name and the corresponding password for this script to execute.\n")
     exit(1)
 
 #  STEP-2a: Retrieve the IP address of the FPGA module (this will be the SERVER)
 # -----------------------------------------------------------------------------
-# OBSOLETE_20200108 ipFpgaStr = getFpgaIpv4(args)
-# OBSOLETE_20200108 ipFpgaStr = args.fpga_ipv4
-# OBSOLETE_20200108 if ipFpgaStr == '':
-# OBSOLETE_20200108     print("Enter the IPv4 address of the FPGA module to connect to (e.g. 10.12.200.21)")
-# OBSOLETE_20200108     ipDaList = read_ipv4_from_console()
-# OBSOLETE_20200108 else:
-# OBSOLETE_20200108     ipDaList = ipStrToList(ipFpgaStr)
-# OBSOLETE_20200108     if len(ipDaList) != 4:
-# OBSOLETE_20200108         print("ERROR: Unrecognized IPv4 address.\n")
-# OBSOLETE_20200108         exit(1)
 ipFpga = getFpgaIpv4(args)
-print(">>>> ipFpga = %s = %d " % (str(ipFpga), int(ipFpga)))
 
-#  STEP-2b: Retrieve the instance Id assigned by the cF Resource Manager
+#  STEP-2b: Retrieve the instance Id assigned by the cloudFPGA Resource Manager
 # -----------------------------------------------------------------------------
-instId = args.inst_id
-if instId == 0:
-    print("Enter the instance Id that was assigned by the cloudFPGA Resource Manager (e.g. 1-32)")
-    instId = read_instance_id_from_console()
+instId = getInstanceId(args)
 
 #  STEP-2c: Retrieve the IP address of the cF Resource Manager
 # -----------------------------------------------------------------------------
-ipResManStr = args.mngr_ipv4
-if ipResManStr == '':
-    print("Enter the IP address of the cloudFPGA Resource Manager (e.g. 10.12.0.132)")
-    ipResManList = read_ipv4_from_console()
-else:
-    ipResManList = ipStrToList(ipResManStr)
-    if len(ipResManList) != 4:
-        print("ERROR: Unrecognized IPv4 address.\n")
-        exit(1)
+ipResMngr = getResourceManagerIpv4(args)
 
-#  STEP-3a: Retrieve the TCP destination port of the FPGA server
+#  STEP-3a: Retrieve the TCP port of the FPGA server
 # -----------------------------------------------------------------------------
-portFpga = args.fpga_port
-if portFpga != 8803:
-    print("ERROR: The current version of the cFp_BringUp role always listens on port #8803.\n")
-    exit(1)
+portFpga = getFpgaPort(args)
 
-#  STEP-3b: Ask the TCP port of the cloudFPGA Resource Manager
+#  STEP-3b: Retrieve the TCP port of the cloudFPGA Resource Manager
 # -----------------------------------------------------------------------------
-tcpResMan = args.mngr_port
-if tcpResMan != 8080:
-    print("ERROR: The current version of the cloudFPGA Resource manager always listens on port #8080.\n")
-    exit(1)
+portResMngr = getResourceManagerPort(args)
 
 #  STEP-?: Configure the application registers
 # -----------------------------------------------------------------------------
@@ -251,28 +309,11 @@ if tcpResMan != 8080:
 
 #  STEP-4: Trigger the FPGA role to restart (i.e. perform SW reset of the role)
 # -----------------------------------------------------------------------------
-print("\nNow: Requesting the FPGA application to restart.")
-try:
-    # Build a request that is formatted as follows:
-    #  http://10.12.0.132:8080/instances/13/app_restart?username=fab&password=secret
-    reqUrl = "http://" + ipResManStr + ":" + str(tcpResMan) + "/instances/" \
-                       + str(instId) + "/app_restart?username=" + args.user_name \
-                       + "&password=" + args.password
-    # DEBUG print("Generated request URL = ", reqUrl)
-    r1 = requests.patch(reqUrl)
-    print(r1.content.decode())
-except Exception as e:
-    print("ERROR: Failed to reset the FPGA role")
-    print(str(e))
-    exit(1)
+restartApp(instId, ipResMngr, portResMngr, args.user_name, args.user_passwd)
 
 #  STEP-5: Ping the FPGA
 # -----------------------------------------------------------------------------
-print("\nNow: Trying to \'ping\' the FPGA:")
-rc = os.system("ping -c 2 " + str(ipFpga))
-if rc != 0:
-    print("ERROR: FPGA does not reply to \'ping\'!")
-    exit(1)
+pingFpga(ipFpga)
 
 #  STEP-6a: Set the FPGA socket association
 # -----------------------------------------------------------------------------
@@ -291,25 +332,15 @@ if 0:
     tcpSP = tcpDP + 49152  # 8803 + 0xC000
     hostAssociation = (ipSaStr, tcpSP)
 
-#  STEP-7: Check if current socket is in TIME_WAIT state
-# -------------------------------------------------------
-wait = True
-# Example: rc = os.system("netstat | grep '10.12.200.163:8803' | grep TIME_WAIT")
-cmdStr = "netstat | grep \'" + str(ipFpga) + ":" + str(portFpga) + "\' | grep \'TIME_WAIT\|FIN_WAIT\' "
-while wait:
-    rc = os.system(cmdStr)
-    if rc == 0:
-        print("[INFO] Cannot reuse this socket as long as it is in the \'TIME_WAIT\' or \'FIN_WATIT\' state.")
-        print("       Let's sleep for 5 sec...")
-        time.sleep(5)
-    else:
-        wait = False
+#  STEP-7: Wait until the current socket can be reused
+# -----------------------------------------------------------------------------
+waitUntilSocketPairCanBeReused(ipFpga, portFpga)
 
 #  STEP-8a: Create a TCP/IP socket for the TCP/IP connection
 # -----------------------------------------------------------------------------
 try:
     tcpSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-except Exception as exc:  # socket.error as exc:
+except Exception as exc:
     print("[EXCEPTION] %s" % exc)
     exit(1)
 
@@ -321,7 +352,7 @@ if 0:
     try:
         tcpSock.bind(hostAssociation)
         print('Binding the socket address of the HOST to {%s, %d}' % hostAssociation)
-    except Exception as exc:  # socket.error as exc:
+    except Exception as exc:
         print("[EXCEPTION] %s" % exc)
         exit(1)
 
@@ -340,15 +371,15 @@ else:
 size = 128
 count = args.loop_count
 
-if 0:
-    message = str_rand_gen(size)
-else:
+if args.verbose:
     message = str_static_gen(size)
+else:
+    message = str_rand_gen(size)
 
 #  STEP-11: Create Rx and Tx threads
 # ----------------------------------
-tx_thread = threading.Thread(target=tcp_tx, args=(tcpSock, message, count))
-rx_thread = threading.Thread(target=tcp_rx, args=(tcpSock, message, count))
+tx_thread = threading.Thread(target=tcp_tx, args=(tcpSock, message, count, args.verbose))
+rx_thread = threading.Thread(target=tcp_rx, args=(tcpSock, message, count, args.verbose))
 
 #  STEP-12: Start the threads
 # ---------------------------
