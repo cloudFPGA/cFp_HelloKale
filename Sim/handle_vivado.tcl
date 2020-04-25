cca3d2d7fd946958e62c258f065ddf5d31c35729
Mitra <mpu@dev0.ngl.zc2.ibm.com>
2020-04-25 16:10:06 +0000

    ip

--- Sim/handle_vivado.tcl
+++ Sim/handle_vivado.tcl
@@ -47,11 +47,12 @@ set dbgLvl_3         3
 #                        *     *  *    *     *    *    *                       #
 #                                                                              #
 ################################################################################
-
 open_project ${xprDir}/${xprName}.xpr
+
 set_property SOURCE_SET sources_1 [get_filesets sim_1]
 set_property -name {xsim.simulate.runtime} -value {50us} -objects [get_filesets sim_1]
 
+reset_simulation
 export_ip_user_files -of_objects  [get_files ${rootDir}/TOP/hdl/top.vhdl] -no_script -reset -force -quiet
 remove_files ${rootDir}/TOP/hdl/top.vhdl
 update_compile_order -fileset sources_1
@@ -71,4 +72,23 @@ update_compile_order -fileset sources_1
 add_files -fileset sim_1 -norecurse ${rootDir}/Sim/tb_topFMKU60.sv
 update_compile_order -fileset sim_1
 update_compile_order -fileset sim_1
+
+generate_target Simulation [get_files ${rootDir}/ROLE/ip/MemTestFlash/MemTestFlash.xci]
+export_ip_user_files -of_objects [get_files ${rootDir}/ROLE/ip/MemTestFlash/MemTestFlash.xci] -no_script -sync -force -quiet
+export_simulation -of_objects [get_files ${rootDir}/ROLE/ip/MemTestFlash/MemTestFlash.xci] -directory ${rootDir}/xpr/topFMKU60.ip_user_files/sim_scripts -ip_user_files_dir ${rootDir}/xpr/topFMKU60.ip_user_files -ipstatic_source_dir ${rootDir}/xpr/topFMKU60.ip_user_files/ipstatic -lib_map_path [list {modelsim=${rootDir}/xpr/topFMKU60.cache/compile_simlib/modelsim} {questa=${rootDir}/xpr/topFMKU60.cache/compile_simlib/questa} {ies=${rootDir}/xpr/topFMKU60.cache/compile_simlib/ies} {vcs=${rootDir}/xpr/topFMKU60.cache/compile_simlib/vcs} {riviera=${rootDir}/xpr/topFMKU60.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+
+generate_target Simulation [get_files ${rootDir}/ROLE/ip/TcpApplicationFlash/TcpApplicationFlash.xci]
+
+export_ip_user_files -of_objects [get_files ${rootDir}/ROLE/ip/TcpApplicationFlash/TcpApplicationFlash.xci] -no_script -sync -force -quiet
+export_simulation -of_objects [get_files ${rootDir}/ROLE/ip/TcpApplicationFlash/TcpApplicationFlash.xci] -directory ${rootDir}/xpr/topFMKU60.ip_user_files/sim_scripts -ip_user_files_dir ${rootDir}/xpr/topFMKU60.ip_user_files -ipstatic_source_dir ${rootDir}/xpr/topFMKU60.ip_user_files/ipstatic -lib_map_path [list {modelsim=${rootDir}/xpr/topFMKU60.cache/compile_simlib/modelsim} {questa=${rootDir}/xpr/topFMKU60.cache/compile_simlib/questa} {ies=${rootDir}/xpr/topFMKU60.cache/compile_simlib/ies} {vcs=${rootDir}/xpr/topFMKU60.cache/compile_simlib/vcs} {riviera=${rootDir}/xpr/topFMKU60.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+generate_target Simulation [get_files ${rootDir}/ROLE/ip/UdpApplicationFlash/UdpApplicationFlash.xci]
+
+export_ip_user_files -of_objects [get_files ${rootDir}/ROLE/ip/UdpApplicationFlash/UdpApplicationFlash.xci] -no_script -sync -force -quiet
+export_simulation -of_objects [get_files ${rootDir}/ROLE/ip/UdpApplicationFlash/UdpApplicationFlash.xci] -directory ${rootDir}/xpr/topFMKU60.ip_user_files/sim_scripts -ip_user_files_dir ${rootDir}/xpr/topFMKU60.ip_user_files -ipstatic_source_dir ${rootDir}/xpr/topFMKU60.ip_user_files/ipstatic -lib_map_path [list {modelsim=${rootDir}/xpr/topFMKU60.cache/compile_simlib/modelsim} {questa=${rootDir}/xpr/topFMKU60.cache/compile_simlib/questa} {ies=${rootDir}/xpr/topFMKU60.cache/compile_simlib/ies} {vcs=${rootDir}/xpr/topFMKU60.cache/compile_simlib/vcs} {riviera=${rootDir}/xpr/topFMKU60.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+generate_target Simulation [get_files ${rootDir}/ROLE/ip/TcpShellInterface/TcpShellInterface.xci]
+
+export_ip_user_files -of_objects [get_files ${rootDir}/ROLE/ip/TcpShellInterface/TcpShellInterface.xci] -no_script -sync -force -quiet
+export_simulation -of_objects [get_files ${rootDir}/ROLE/ip/TcpShellInterface/TcpShellInterface.xci] -directory ${rootDir}/xpr/topFMKU60.ip_user_files/sim_scripts -ip_user_files_dir ${rootDir}/xpr/topFMKU60.ip_user_files -ipstatic_source_dir ${rootDir}/xpr/topFMKU60.ip_user_files/ipstatic -lib_map_path [list {modelsim=${rootDir}/xpr/topFMKU60.cache/compile_simlib/modelsim} {questa=${rootDir}/xpr/topFMKU60.cache/compile_simlib/questa} {ies=${rootDir}/xpr/topFMKU60.cache/compile_simlib/ies} {vcs=${rootDir}/xpr/topFMKU60.cache/compile_simlib/vcs} {riviera=${rootDir}/xpr/topFMKU60.cache/compile_simlib/riviera}] -use_ip_compiled_libs -force -quiet
+generate_target Simulation [get_files ${rootDir}/ip/TenGigEthSubSys_X1Y8_R/TenGigEthSubSys_X1Y8_R.xci]
+
 launch_simulation
