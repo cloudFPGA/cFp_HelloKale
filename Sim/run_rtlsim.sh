820910787e56a46c9b8dec4864afd7f838130d40
Mitra <mpu@dev0.ngl.zc2.ibm.com>
2020-04-29 11:06:45 +0000

    check

--- Sim/run_rtlsim.sh
+++ Sim/run_rtlsim.sh
@@ -1,3 +1,4 @@
 #vivado -mode batch -source Sim/top.handle_vivado.tcl -notrace -log handle_vivado.synth.log -tclargs -full_src -force -create -synth
+$PWD
 vivado -mode batch -source Sim/top.handle_vivado.tcl -notrace -log handle_vivado.sim.log -tclargs -full_src -force -RTLsim
 #vivado -mode batch -source Sim/handle_vivado.tcl -notrace -log Sim/handle_vivado.log
