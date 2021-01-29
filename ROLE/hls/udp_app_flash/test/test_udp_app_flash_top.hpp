474c02ccdfbe999dc3db7cad0606778cc0cacf62
Francois Abel <fab@zurich.ibm.com>
2021-01-29 22:03:40 +0100

    UAF - Adding missing file.

---
+++ ROLE/hls/udp_app_flash/test/test_udp_app_flash_top.hpp
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 -- 2020 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*******************************************************************************
+ * @file       : test_udp_app_flash_top.hpp
+ * @brief      : Testbench for toplevel of the UDP Application Flash (UAF).
+ *
+ * System:     : cloudFPGA
+ * Component   : cFp_Monolithic / ROLE
+ * Language    : Vivado HLS
+ *
+ * \ingroup ROLE_UAF
+ * \addtogroup ROLE_UAF_TEST
+ * \{
+ *******************************************************************************/
+
+#ifndef _TEST_UAF_TOP_H_
+#define _TEST_UAF_TOP_H_
+
+#include "../src/udp_app_flash_top.hpp"
+#include "../../../../cFDK/SRA/LIB/SHELL/LIB/hls/NTS/nts_utils.hpp"
+#include "../../../../cFDK/SRA/LIB/SHELL/LIB/hls/NTS/SimNtsUtils.hpp"
+#include "../../../../cFDK/SRA/LIB/SHELL/LIB/hls/NTS/SimUdpDatagram.hpp"
+
+//---------------------------------------------------------
+//-- TESTBENCH GLOBAL VARIABLES
+//---------------------------------------------------------
+unsigned int    gSimCycCnt    = 0;
+bool            gTraceEvent   = false;
+bool            gFatalError   = false;
+unsigned int    gMaxSimCycles = 42;
+
+#endif
+
+/*! \} */
+
+
+
+
+
