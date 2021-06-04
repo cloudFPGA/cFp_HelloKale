17eff4122d366936e46c9d688e50412575de7341
Francois Abel <fab@zurich.ibm.com>
2021-06-04 19:09:26 +0200

    Cleanup

--- ROLE/hls/udp_app_flash/src/udp_app_flash_top.cpp
+++ ROLE/hls/udp_app_flash/src/udp_app_flash_top.cpp
@@ -40,40 +40,9 @@ using namespace hls;
 
 /************************************************
  * INTERFACE SYNTHESIS DIRECTIVES
- *  For the time being, we continue to design
- *  with the DEPRECATED directives because the
- *  new PRAGMAs do not always work for us.
  ************************************************/
-#undef  USE_DEPRECATED_DIRECTIVES
-#define USE_AP_FIFO
-
-/*** OBSOLETE_20210222 ****************
-template <class Type>
-void pAxisToFifo(
-        stream<Type>& siAxisStream,
-        stream<Type>& soFifoStream)
-{
-    #pragma HLS INLINE off
-    Type currChunk;
-    siAxisStream.read(currChunk);  // Blocking read
-    if (!soFifoStream.full()) {
-        soFifoStream.write(currChunk);  // else drop
-    }
-}
-
-template <class Type>
-void pFifoToAxis(
-        stream<Type>& siFifoStream,
-        stream<Type>& soAxisStream)
-{
-    #pragma HLS INLINE off
-    Type currChunk;
-    if (!siFifoStream.empty()) {
-        siFifoStream.read(currChunk);
-        soAxisStream.write(currChunk);  // Blocking write
-    }
-}
-***************************************/
+#define UAF_USE_AP_FIFO
+#undef  UAF_USE_NON_FIFO_IO
 
 /*******************************************************************************
  * @brief   Top of UDP Application Flash (UAF)
@@ -87,28 +56,25 @@ void pFifoToAxis(
  * @param[out] soUSIF_Meta          UDP metadata to [USIF].
  * @param[out] soUSIF_DLen          UDP data len to [USIF].
  *
- * @info This toplevel exemplifies the instantiation of a core that was designed
- *   for non-blocking read and write streams, but which needs to be exported as
- *   an IP with AXI-Stream interfaces (axis).
- *   Because the 'axis' interface does not support non-blocking accesses, this
- *   toplevel implements a shim layer between the 'axis' and the 'ap_fifo'
- *   interfaces and vice versa.
+ * @info This toplevel exemplifies the instantiation of a core that uses AP_FIFO
+ *   interfaces instead of AXIS.
+ *
  *******************************************************************************/
-void udp_app_flash_top (
-
+#if HLS_VERSION == 2016
+    void udp_app_flash_top (
         //------------------------------------------------------
         //-- SHELL / Mmio / Configuration Interfaces
         //------------------------------------------------------
-        //[NOT_USED] ap_uint<2>  piSHL_Mmio_EchoCtrl,
-        //[NOT_USED] ap_uint<1>  piSHL_Mmio_PostPktEn,
-        //[NOT_USED] ap_uint<1>  piSHL_Mmio_CaptPktEn,
-
+    #if defined UAF_USE_NON_FIFO_IO
+        ap_uint<2>  piSHL_Mmio_EchoCtrl,
+        ap_uint<1>  piSHL_Mmio_PostPktEn,
+        ap_uint<1>  piSHL_Mmio_CaptPktEn,
+    #endif
         //------------------------------------------------------
         //-- USIF / Rx Data Interfaces
         //------------------------------------------------------
         stream<UdpAppData>  &siUSIF_Data,
         stream<UdpAppMetb>  &siUSIF_Meta,
-
         //------------------------------------------------------
         //-- USIF / Tx Data Interfaces
         //------------------------------------------------------
@@ -119,41 +85,80 @@ void udp_app_flash_top (
     //-- DIRECTIVES FOR THE INTERFACES ----------------------------------------
     #pragma HLS INTERFACE ap_ctrl_none port=return
 
-#if defined(USE_DEPRECATED_DIRECTIVES)
-    //-- DIRECTIVES FOR THE INTERFACES -----------------------------------------
-
     /*********************************************************************/
     /*** For the time being, we continue designing with the DEPRECATED ***/
     /*** directives because the new PRAGMAs do not work for us.        ***/
     /*********************************************************************/
-
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_EchoCtrl
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_PostPktEn
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_CaptPktEn
-
+  #if defined UAF_USE_NON_FIFO_IO
+    #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_EchoCtrl
+    #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_PostPktEn
+    #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_CaptPktEn
+  #endif
     #pragma HLS resource core=AXI4Stream variable=siUSIF_Data    metadata="-bus_bundle siUSIF_Data"
     #pragma HLS resource core=AXI4Stream variable=siUSIF_Meta    metadata="-bus_bundle siUSIF_Meta"
     #pragma HLS DATA_PACK                variable=siUSIF_Meta
-
     #pragma HLS resource core=AXI4Stream variable=soUSIF_Data    metadata="-bus_bundle soUSIF_Data"
     #pragma HLS resource core=AXI4Stream variable=soUSIF_Meta    metadata="-bus_bundle soUSIF_Meta"
     #pragma HLS DATA_PACK                variable=soUSIF_Meta
     #pragma HLS resource core=AXI4Stream variable=soUSIF_DLen    metadata="-bus_bundle soUSIF_DLen"
 
-#else
-    //-- DIRECTIVES FOR THE INTERFACES -----------------------------------------
-  #if defined (USE_AP_FIFO)
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable register port=piSHL_Mmio_EchoCtrl  name=piSHL_Mmio_EchoCtrl
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_PostPktEn
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_CaptPktEn
+    //-- DIRECTIVES FOR THIS PROCESS -------------------------------------------
+    #pragma HLS DATAFLOW
 
+    //-- INSTANTIATE TOPLEVEL --------------------------------------------------
+    udp_app_flash (
+        //-- SHELL / Mmio / Configuration Interfaces
+  #if defined UAF_USE_NON_FIFO_IO
+      piSHL_Mmio_EchoCtrl,
+      piSHL_Mmio_PostPktEn,
+      piSHL_Mmio_CaptPktEn,
+  #endif
+        //-- USIF / Rx Data Interfaces
+        siUSIF_Data,
+        siUSIF_Meta,
+        //-- USIF / Tx Data Interfaces
+        soUSIF_Data,
+        soUSIF_Meta,
+        soUSIF_DLen);
+}
+#else
+    void udp_app_flash_top (
+        //------------------------------------------------------
+        //-- SHELL / Mmio / Configuration Interfaces
+        //------------------------------------------------------
+    #if defined UAF_USE_NON_FIFO_IO
+        ap_uint<2>  piSHL_Mmio_EchoCtrl,
+        ap_uint<1>  piSHL_Mmio_PostPktEn,
+        ap_uint<1>  piSHL_Mmio_CaptPktEn,
+    #endif
+        //------------------------------------------------------
+        //-- USIF / Rx Data Interfaces
+        //------------------------------------------------------
+        stream<UdpAppData>  &siUSIF_Data,
+        stream<UdpAppMetb>  &siUSIF_Meta,
+        //------------------------------------------------------
+        //-- USIF / Tx Data Interfaces
+        //------------------------------------------------------
+        stream<UdpAppData>  &soUSIF_Data,
+        stream<UdpAppMetb>  &soUSIF_Meta,
+        stream<UdpAppDLen>  &soUSIF_DLen)
+{
+    //-- DIRECTIVES FOR THE INTERFACES ----------------------------------------
+    #pragma HLS INTERFACE ap_ctrl_none port=return
+
+  #if defined TAF_USE_NON_FIFO_IO
+    #pragma HLS INTERFACE ap_stable register port=piSHL_Mmio_EchoCtrl  name=piSHL_Mmio_EchoCtrl
+    #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_PostPktEn
+    #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_CaptPktEn
+  #endif
+
+  #if defined (UAF_USE_AP_FIFO)
     //-- [USIF] INTERFACES ------------------------------------------------------
     #pragma HLS INTERFACE ap_fifo   port=siUSIF_Data    name=siUSIF_Data
     #pragma HLS DATA_PACK       variable=siUSIF_Data
     #pragma HLS INTERFACE ap_fifo   port=siUSIF_Meta    name=siUSIF_Meta
     // [WARNING] Do not pack 'siUSIF_Meta' because the DATA_PACK optimization
     //    does not support packing structs which contain other structs!!!
-
     #pragma HLS INTERFACE ap_fifo   port=soUSIF_Data    name=soUSIF_Data
     #pragma HLS DATA_PACK       variable=soUSIF_Data
     #pragma HLS INTERFACE ap_fifo   port=soUSIF_Meta    name=soUSIF_Meta
@@ -161,10 +166,6 @@ void udp_app_flash_top (
     //    does not support packing structs which contain other structs!!!
     #pragma HLS INTERFACE ap_fifo   port=soUSIF_DLen    name=soUSIF_DLen
   #else
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable register port=piSHL_Mmio_EchoCtrl  name=piSHL_Mmio_EchoCtrl
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_PostPktEn
-    //[NOT_USED] #pragma HLS INTERFACE ap_stable port=piSHL_Mmio_CaptPktEn
-
     //-- [USIF] INTERFACES ------------------------------------------------------
     #pragma HLS INTERFACE axis  register off   port=siUSIF_Data    name=siUSIF_Data
     #pragma HLS INTERFACE axis  register off   port=siUSIF_Meta    name=siUSIF_Meta
@@ -176,7 +177,6 @@ void udp_app_flash_top (
     //    does not support packing structs which contain other structs!!!
     #pragma HLS INTERFACE axis  register off   port=soUSIF_DLen    name=soUSIF_DLen
   #endif
-#endif
 
     //-- DIRECTIVES FOR THIS PROCESS -------------------------------------------
 #if HLS_VERSION == 2017
@@ -188,24 +188,22 @@ void udp_app_flash_top (
 
     //-- INSTANTIATE TOPLEVEL --------------------------------------------------
     udp_app_flash (
-        //------------------------------------------------------
         //-- SHELL / Mmio / Configuration Interfaces
-        //------------------------------------------------------
-        //[NOT_USED] piSHL_Mmio_EchoCtrl,
-        //[NOT_USED] piSHL_Mmio_PostPktEn,
-        //[NOT_USED] piSHL_Mmio_CaptPktEn,
-        //------------------------------------------------------
+    #if defined UAF_USE_NON_FIFO_IO
+        piSHL_Mmio_EchoCtrl,
+        piSHL_Mmio_PostPktEn,
+        piSHL_Mmio_CaptPktEn,
+    #endif
         //-- USIF / Rx Data Interfaces
-        //------------------------------------------------------
         siUSIF_Data,
         siUSIF_Meta,
-        //------------------------------------------------------
         //-- USIF / Tx Data Interfaces
-        //------------------------------------------------------
         soUSIF_Data,
         soUSIF_Meta,
         soUSIF_DLen);
 
 }
+
+#endif  //  HLS_VERSION
 
 /*! \} */
